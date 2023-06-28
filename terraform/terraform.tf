7edf26a398603706c00195e9663d27ba6f0d05f4
LazarGrbic11 <grbiclazar18@gmail.com>
2023-06-28 21:25:03 +0200

    Added .tf file, used to create the instance

---
+++ terraform/terraform.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = ""
+}
+
+resource "digitalocean_volume" "testvolume" {
+  name      = "testvolume"
+  region    = "fra1"
+  size      = 25
+  filesystem_type = "ext4"
+}
+
+resource "digitalocean_droplet" "testprojekat" {
+  name           = "jenkins-droplet"
+  region         = "fra1"
+  size           = "s-2vcpu-2gb"
+  image          = "ubuntu-18-04-x64"
+  volume_ids     = [digitalocean_volume.testvolume.id]
+}
+output "droplet_ip" {
+  value = digitalocean_droplet.testprojekat.ipv4_address
+}
