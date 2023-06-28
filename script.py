75ae6d4bc8bd0629a2a107bd3fca9facf52ee069
LazarGrbic11 <grbiclazar18@gmail.com>
2023-06-28 20:52:38 +0200

    Updated script.py

--- script.py
+++ script.py
@@ -3,8 +3,3 @@ import subprocess
 cmd = "cp /var/lib/jenkins/workspace/job1/index.html ~/nginx/html"
 subprocess.run(cmd, shell=True)
 
-cmd = "cd ~/nginx"
-subprocess.run(cmd, shell=True)
-
-cmd = "docker-compose restart"
-subprocess.run(cmd, shell=True)
